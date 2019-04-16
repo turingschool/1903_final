f03d2938907ab3bd192bd8f1a8639687f58e3b33
davisc0801 <davisc1014@gmail.com>
2019-04-16 09:35:42 -0600

    Add curator test setup

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -5,4 +5,34 @@ require "./lib/curator"
 
 class CuratorTest < Minitest::Test
 
+  def setup
+    @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
+  end
+
 end
