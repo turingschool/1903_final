0998222a372fa1b134909afa6926d17e5475b7f3
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-16 09:28:15 -0600

    All photograph tests passing

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,1 +1,10 @@
-c
+class Photograph
+  attr_reader :id, :name, :artist_id, :year
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @name = attributes[:name]
+    @artist_id = attributes[:artist_id]
+    @year = attributes[:year]
+  end
+end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -1,22 +1,28 @@
-#
-# pry(main)> attributes = {
-#   id: "1",
-#   name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#   artist_id: "4",
-#   year: "1954"
-# }
-# 
-# pry(main)> photograph = Photograph.new(attributes)
-# #=> #<Photograph:0x00007fc2d1050c80...>
-#
-# pry(main)> photograph.id
-# #=> "1"
-#
-# pry(main)> photograph.name
-# #=> "Rue Mouffetard, Paris (Boy with Bottles)"
-#
-# pry(main)> photograph.artist_id
-# #=> "4"
-#
-# pry(main)> photograph.year
-# #=> "1954"
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+
+class PhotographerTest < Minitest::Test
+
+  def setup
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    }
+    @photograph = Photograph.new(attributes)
+  end
+
+  def test_it_exists
+    assert_instance_of Photograph, @photograph
+  end
+
+  def test_it_has_attributes
+    assert_equal "1",  @photograph.id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @photograph.name
+    assert_equal "4", @photograph.artist_id
+    assert_equal "1954", @photograph.year
+  end
+end
