a7e722b5e1f7d2565ed4984c57313c93860f28c0
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-16 09:22:55 -0600

    Add init of Curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,2 +1,7 @@
 class Curator
+  attr_reader :photographs
+
+  def initialize
+    @photographs = []
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -10,4 +10,8 @@ class CuratorTest < Minitest::Test
   def test_it_exists
     assert_instance_of Curator, @curator
   end
+
+  def test_it_inits_with_empty_ary_of_photographs
+    assert_equal [], @curator.photographs
+  end
 end
