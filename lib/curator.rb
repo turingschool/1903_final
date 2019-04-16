be50d37a4a8f1c06ef93a00b5e8c3eed7d883d8a
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-16 09:43:55 -0600

    Add #find_photograph_by_id method to Curator, update test to have photos, typo

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,4 +13,10 @@ class Curator
   def add_artist(artist)
     artists << artist
   end
+
+  def find_photograph_by_id(find_id)
+    photographs.find do |photo|
+      photo.id == find_id
+    end
+  end
 end
