e2c273e2a55e1bbbd18e661f7b96663658ebfdde
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-04-16 10:11:33 -0600

    Iteration 3 complete

--- lib/curator.rb
+++ lib/curator.rb
@@ -27,5 +27,32 @@ class Curator
     end
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+      artist.id == photo.artist_id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    photo_by_artist = []
+    @photographs.sort_by do |photo|
+      photo_by_artist << photo.artist_id
+    end
+
+    artist = []
+    photo_by_artist.find do |id|
+      if photo_by_artist.count(id) > 1
+         artist << find_artist_by_id(id)
+      end
+    end
+    artist
+  end
+
+  def photographs_taken_by_artist_from(country)
+    @artists.find_all do |artist|
+      artist.country == country
+    end
+  end
+
 
 end
