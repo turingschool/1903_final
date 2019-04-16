a56a5e1855741ebb8450345e70dc40daf7c35eb8
Brian Plantico <bplantico@gmail.com>
2019-04-16 09:36:04 -0600

    Adds Artist class which completes Iteration 1

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -27,29 +27,3 @@ class PhotographTest < Minitest::Test
   end
 
 end
-
-# pry(main)> attributes = {
-#   id: "2",
-#   name: "Ansel Adams",
-#   born: "1902",
-#   died: "1984",
-#   country: "United States"
-# }
-#
-# pry(main)> artist = Artist.new(attributes)
-# #=> #<Artist:0x00007fc2d0a6c080...>
-#
-# pry(main)> artist.id
-# #=> "2"
-#
-# pry(main)> artist.name
-# #=> "Ansel Adams"
-#
-# pry(main)> artist.born
-# #=> "1902"
-#
-# pry(main)> artist.died
-# #=> "1984"
-#
-# pry(main)> artist.country
-# #=> "United States"
