22824f59d7a79da0159f18de413be40ce2f73f54
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-16 09:46:59 -0600

    Iteration 2 complete

--- lib/curator.rb
+++ lib/curator.rb
@@ -17,6 +17,12 @@ class Curator
   def find_artist_by_id(id)
     @artists.find do |artist|
       artist.id == id
-    end 
+    end
   end
+
+  def find_photograph_by_id(id)
+    @photographs.find do |photo|
+      photo.id == id
+    end
+  end 
 end
