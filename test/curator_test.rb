2ecadec95e32168a50ac9f3fa2a77fb5a884db93
Justin <justin@baal-software.com>
2019-04-16 09:42:12 -0600

    Add curator initial tests

---
+++ test/curator_test.rb
@@ -0,0 +1,61 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/photograph'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+  attr_reader :curator, :photo_1, :photo_2, :artist_1, :artist_2
+
+  def setup
+    @curator = Curator.new
+    @photo_1 = Photograph.new({
+                               id: "1",
+                               name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                               artist_id: "1",
+                               year: "1954"
+                             })
+    @photo_2 = Photograph.new({
+                               id: "2",
+                               name: "Moonrise, Hernandez",
+                               artist_id: "2",
+                               year: "1941"
+                              })
+    @artist_1 = Artist.new({
+                            id: "1",
+                            name: "Henri Cartier-Bresson",
+                            born: "1908",
+                            died: "2004",
+                            country: "France"
+                           })
+    @artist_2 = Artist.new({
+                            id: "2",
+                            name: "Ansel Adams",
+                            born: "1902",
+                            died: "1984",
+                            country: "United States"
+                          })
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, curator
+  end
+
+  def test_it_has_attributes
+    assert_equal [], curator.photographs
+    assert_equal [], curator.artists
+  end
+
+  def test_it_can_add_photographs
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+
+    assert_equal [photo_1, photo_2], curator.photographs
+  end
+
+  def test_it_can_add_artists
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+
+    assert_equal [artist_1, artist_2], curator.artists
+  end
+end
