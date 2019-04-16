4c6c63186e9de33b63e36c5dc802a97588e5ff4f
lpile <logan.pile@gmail.com>
2019-04-16 09:33:51 -0600

    adds curator_test

---
+++ test/curator_test.rb
@@ -0,0 +1,62 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/photograph'
+require './lib/artist'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+  def setup
+    @curator = Curator.new
+    @photo_1 = Photograph.new({id: "1",
+                               name: "Rue Mouffetard, Paris (Boy with Bottles)",
+                               artist_id: "1",
+                               year: "1954"})
+    @photo_2 = Photograph.new({id: "2",
+                               name: "Moonrise, Hernandez",
+                               artist_id: "2",
+                               year: "1941"})
+    @artist_1 = Artist.new({id: "1",
+                            name: "Henri Cartier-Bresson",
+                            born: "1908",
+                            died: "2004",
+                            country: "France"})
+    @artist_2 = Artist.new({id: "2",
+                            name: "Ansel Adams",
+                            born: "1902",
+                            died: "1984",
+                            country: "United States"})
+  end
+
+  def test_it_class_exist
+    assert_instance_of Curator, @curator
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
+  end
+
+  def test_it_can_add_photograph
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_can_add_artist
+    @curator.add_artist(@photo_1)
+    @curator.add_artist(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.artists
+  end
+
+  def test_it_can_find_artist_by_id
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+  end
+
+  def test_it_can_find_photograph_by_id
+    assert_equal @photo_2, @curator.find_artist_by_id("2")
+  end
+end
