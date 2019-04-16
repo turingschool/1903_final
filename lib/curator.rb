8d1bdad265a216d41f8a59003280c5e45dc4b01c
lpile <logan.pile@gmail.com>
2019-04-16 10:20:06 -0600

    updates curator class - Iteration 3 complete

--- lib/curator.rb
+++ lib/curator.rb
@@ -21,4 +21,26 @@ class Curator
   def find_photograph_by_id(id)
     @photographs.find{|photograph| photograph.id == id}
   end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all{|photo| artist.id == photo.artist_id}
+  end
+
+  def artists_with_multiple_photographs
+    artist_work_hash.select{|artist, work| work.count > 1}.keys
+  end
+
+  def photographs_taken_by_artists_from(country)
+    artist_work_hash.select do |artist, work|
+      artist.country == country
+    end.values.flatten
+  end
+
+  def artist_work_hash
+    hash = Hash.new{|hash, key| hash[key] = []}
+    @artists.each do |artist|
+      @photographs.each{|photo| hash[artist] << photo if photo.artist_id == artist.id}
+    end
+    hash
+  end
 end
