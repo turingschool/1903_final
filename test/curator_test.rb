1b2440d4e6f340517261ada1da4921cbc3175d9e
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-16 09:45:49 -0600

    Add most of iteration 2

---
+++ test/curator_test.rb
@@ -0,0 +1,74 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+
+  def setup
+    @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+
+  def test_curator_begins_without_photographs
+    assert_equal [], @curator.photographs
+  end
+
+  def test_it_shows_photographs_as_part_of_collection
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_curator_doesnt_start_with_artists_either
+    assert_equal [], @curator.artists
+  end
+
+  def test_it_shows_artists_now_in_collection
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_it_can_find_artist_by_their_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+  end
+
+  def test_it_can_find_photograph_by_id
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    assert_equal @photo_2, @curator.find_photograph_by_id("2")
+  end
+end
