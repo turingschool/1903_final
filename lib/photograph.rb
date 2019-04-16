339581d9f19af70a09522cf18066bcc648d664de
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-16 09:17:02 -0600

    Add init of Photograph

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,13 @@
 class Photograph
+  attr_reader :id,
+              :name,
+              :artist_id,
+              :year
+
+  def initialize(attributes_hash)
+    @id = attributes_hash[:id]
+    @name = attributes_hash[:name]
+    @artist_id = attributes_hash[:artist_id]
+    @year = attributes_hash[:year]
+  end
 end
