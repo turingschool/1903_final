7435dbd32f5f32b1fd4fd1548aedae227351259c
Vince Carollo <vincecarollo@gmail.com>
2019-04-16 09:38:41 -0600

    Add curator class and tests

---
+++ lib/curator.rb
@@ -0,0 +1,25 @@
+class Curator
+  attr_reader :photographs,
+              :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photo)
+    @photographs << photo
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find{|artist| artist.id == id}
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find{|photo| photo.id == id}
+  end
+end
