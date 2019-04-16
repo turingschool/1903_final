63c90edeff235beade79c0ad5dd7d2e88c75ae99
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-16 09:21:55 -0600

    Add Curator class

---
+++ test/curator_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+  def setup
+    @curator = Curator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+end
