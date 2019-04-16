cfe52e955fec8cb50369136c47dbb0db11902875
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-04-16 09:22:36 -0600

    Artist tests all passing

---
+++ test/photograph_test.rb
@@ -0,0 +1,22 @@
+#
+# pry(main)> attributes = {
+#   id: "1",
+#   name: "Rue Mouffetard, Paris (Boy with Bottles)",
+#   artist_id: "4",
+#   year: "1954"
+# }
+# 
+# pry(main)> photograph = Photograph.new(attributes)
+# #=> #<Photograph:0x00007fc2d1050c80...>
+#
+# pry(main)> photograph.id
+# #=> "1"
+#
+# pry(main)> photograph.name
+# #=> "Rue Mouffetard, Paris (Boy with Bottles)"
+#
+# pry(main)> photograph.artist_id
+# #=> "4"
+#
+# pry(main)> photograph.year
+# #=> "1954"
