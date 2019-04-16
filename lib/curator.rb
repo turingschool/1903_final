06870dec680bf186650fc095af0295737317bfcf
davisc0801 <davisc1014@gmail.com>
2019-04-16 09:47:16 -0600

    Add find photo and artist by ID functionality

--- lib/curator.rb
+++ lib/curator.rb
@@ -15,4 +15,11 @@ class Curator
     @artists << artist
   end
 
+  def find_artist_by_id(id)
+    @artists.find{|artist| artist.id == id}
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find{|photo| photo.id == id}
+  end
 end
