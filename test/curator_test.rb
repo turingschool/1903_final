ff76febc550065ea54043408a1e680b314d484e3
lpile <logan.pile@gmail.com>
2019-04-16 10:31:01 -0600

    update curator_test for iteraton 4

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -107,4 +107,32 @@ class CuratorTest < Minitest::Test
     assert_equal [@photo_2, @photo_3, @photo_4], @curator.photographs_taken_by_artists_from("United States")
     assert_equal [], @curator.photographs_taken_by_artists_from("Argentina")
   end
+
+  def test_it_can_load_photographs_csv_files
+    @curator.load_photographs('./data/photographs.csv')
+
+    assert_equal [@photo_1, @photo_2, @photo_3, @photo_4], @curator.photographs
+  end
+
+  def test_it_can_load_artists_csv_files
+    @curator.load_artists('./data/artists.csv')
+
+    assert_equal [@artist_1, @artist_2, @artist_3], @curator.artists
+  end
+
+  def test_it_can_find_photographs_between_range
+    @curator.load_photographs('./data/photographs.csv')
+    @curator.load_artists('./data/artists.csv')
+
+    assert_equal [@photo_1, @photo_3],@curator.photographs_taken_between(1950..1965)
+  end
+
+  def test_it_can_find_artists_photographs_by_age
+    @curator.load_photographs('./data/photographs.csv')
+    @curator.load_artists('./data/artists.csv')
+    diane_arbus = @curator.find_artist_by_id("3")
+    expected = {44=>"Identical Twins, Roselle, New Jersey", 39=>"Child with Toy Hand Grenade in Central Park"}
+
+    assert_equal expected, @curator.artists_photographs_by_age(diane_arbus)
+  end
 end
