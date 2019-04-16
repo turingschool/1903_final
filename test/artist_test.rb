99c3139e36f4222411808299eed15c57a1b9540c
William Homer <billyhomeriii@gmail.com>
2019-04-16 09:27:14 -0600

    Add Artist class with init values

---
+++ test/artist_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require './lib/artist'
+
+class ArtistTest < MiniTest::Test
+  def setup
+    attributes = {
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      }
+    @artist = Artist.new(attributes)
+  end
+
+  def test_does_it_exist
+    assert_instance_of Artist, @artist
+  end
+
+  def test_it_has_init_values
+    assert_equal '2', @artist.id
+    assert_equal 'Ansel Adams', @artist.name
+    assert_equal '1902', @artist.born
+    assert_equal '1984', @artist.died
+    assert_equal 'United States', @artist.country
+  end
+end
