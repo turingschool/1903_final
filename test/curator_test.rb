b975ffdeb4f3b0cdb0919097fc357ad019d6f53d
davisc0801 <davisc1014@gmail.com>
2019-04-16 10:36:58 -0600

    Add tests for CSV reading

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -8,49 +8,76 @@ class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
     @photo_1 = Photograph.new({
-         id: "1",
-         name: "Rue Mouffetard, Paris (Boy with Bottles)",
-         artist_id: "1",
-         year: "1954"
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
     })
     @photo_2 = Photograph.new({
-         id: "2",
-         name: "Moonrise, Hernandez",
-         artist_id: "2",
-         year: "1941"
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
     })
     @photo_3 = Photograph.new({
-         id: "3",
-         name: "Identical Twins, Roselle, New Jersey",
-         artist_id: "3",
-         year: "1967"
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
     })
     @photo_4 = Photograph.new({
-         id: "4",
-         name: "Monolith, The Face of Half Dome",
-         artist_id: "3",
-         year: "1927"
+      id: "4",
+      name: "Monolith, The Face of Half Dome",
+      artist_id: "3",
+      year: "1927"
     })
+    @photo_4_alt = Photograph.new({
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+      })
     @artist_1 = Artist.new({
-        id: "1",
-        name: "Henri Cartier-Bresson",
-        born: "1908",
-        died: "2004",
-        country: "France"
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
     })
     @artist_2 = Artist.new({
-        id: "2",
-        name: "Ansel Adams",
-        born: "1902",
-        died: "1984",
-        country: "United States"
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
     })
     @artist_3 = Artist.new({
-         id: "3",
-         name: "Diane Arbus",
-         born: "1923",
-         died: "1971",
-         country: "United States"
+       id: "3",
+       name: "Diane Arbus",
+       born: "1923",
+       died: "1971",
+       country: "United States"
+    })
+    @artist_4 = Artist.new({
+       id: "4",
+       name: "Walker Evans",
+       born: "1903",
+       died: "1975",
+       country: "United States"
+    })
+    @artist_5 = Artist.new({
+       id: "5",
+       name: "Manuel Alvarez Bravo",
+       born: "1902",
+       died: "2002",
+       country: "Mexico"
+    })
+    @artist_6 = Artist.new({
+       id: "6",
+       name: "Bill Cunningham",
+       born: "1929",
+       died: "2016",
+       country: "United States"
     })
   end
 
@@ -123,6 +150,20 @@ class CuratorTest < Minitest::Test
     @curator.add_artist(@artist_3)
     assert_equal @curator.photographs_taken_by_artist_from("United States"),
     [@photo_2, @photo_3, @photo_4]
+  end
+
+  def test_it_can_load_photographs
+    assert_equal @curator.photographs, []
+    @curator.load_photographs("./data/photographs.csv")
+    assert_equal @curator.photographs,
+    [@photo_1, @photo_2, @photo_3, @photo_4_alt]
+  end
+
+  def test_it_can_load_artists
+    assert_equal @curator.artists, []
+    @curator.load_artists("./data/artists.csv")
+    assert_equal @curator.artists,
+    [@artist_1, @artist_2, @artist_3, @artist_4, @artist_5, @artist_6]
   end
 
 end
