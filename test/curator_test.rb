02f862a7b25783c231c528e42b2c1c3077706f39
Brian Plantico <bplantico@gmail.com>
2019-04-16 09:59:59 -0600

    Adds Curator class through find_artist_by_id method. Next is find_photograph_by_id method

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,8 +1,80 @@
 require 'minitest/autorun'
 require 'minitest/emoji'
+require './lib/photograph'
+require './lib/artist'
 require './lib/curator'
 
 class CuratorTest < Minitest::Test
-  
+
+  def setup
+    @curator = Curator.new
+
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
+
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+
+  def test_photographs_initializes_empty
+    assert_equal [], @curator.photographs
+  end
+
+  def test_add_photo_method
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_artists_initializes_empty
+    assert_equal [], @curator.artists
+  end
+
+  def test_add_artist_method_adds_to_artists_array
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_can_find_artist_by_id
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+  end
 
 end
+
+# pry(main)> curator.find_photograph_by_id("2")
+# # => #<Photograph:0x00007fb3d8e0bfa8...>
