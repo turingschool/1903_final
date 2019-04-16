ff01914ad097b17da94d76702ce0a62526783c20
Justin <justin@baal-software.com>
2019-04-16 10:01:29 -0600

    Add methods for finding photos by artist/country

--- lib/curator.rb
+++ lib/curator.rb
@@ -15,11 +15,22 @@ class Curator
   end
 
   def find_artist_by_id(id)
-    @artists.find { |artist| artist.id == id }
+    @artists.find { |artist| artist.id.eql? id }
   end
 
   def find_photograph_by_id(id)
-    @photographs.find { |photo| photo.id == id }
+    @photographs.find { |photo| photo.id.eql? id }
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.select { |photo| photo.artist_id.eql? artist.id  }
+  end
+
+  def artists_with_multiple_photographs
+    @artists.select { |artist| find_photographs_by_artist(artist).count > 1 }
+  end
+
+  def photographs_taken_by_artist_from(country)
+    @photographs.select { |photo| find_artist_by_id(photo.artist_id).country.eql? country }
+  end
 end
