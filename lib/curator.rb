ee770439a41e32457db43f09cc5206f978fea239
lpile <logan.pile@gmail.com>
2019-04-16 09:41:44 -0600

    adds curator class - Iteration 2 complete

---
+++ lib/curator.rb
@@ -0,0 +1,24 @@
+class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photo)
+    @photographs << photo
+  end
+
+  def add_artist(artist)
+    @artists << artist
+  end
+
+  def find_artist_by_id(id)
+    @artists.find{|artist| artist.id == id}
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find{|photograph| photograph.id == id}
+  end
+end
