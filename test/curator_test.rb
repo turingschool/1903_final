e209c6a010c371d2faea96eb8430b0f6d6fc86ab
lpile <logan.pile@gmail.com>
2019-04-16 09:55:41 -0600

    update curator_test for iteration 3

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -17,6 +17,14 @@ class CuratorTest < Minitest::Test
                                name: "Moonrise, Hernandez",
                                artist_id: "2",
                                year: "1941"})
+    @photo_3 = Photograph.new({id: "3",
+                               name: "Identical Twins, Roselle, New Jersey",
+                               artist_id: "3",
+                               year: "1967"})
+    @photo_4 = Photograph.new({id: "4",
+                               name: "Monolith, The Face of Half Dome",
+                               artist_id: "3",
+                               year: "1927"})
     @artist_1 = Artist.new({id: "1",
                             name: "Henri Cartier-Bresson",
                             born: "1908",
@@ -27,6 +35,21 @@ class CuratorTest < Minitest::Test
                             born: "1902",
                             died: "1984",
                             country: "United States"})
+    @artist_3 = Artist.new({id: "3",
+                            name: "Diane Arbus",
+                            born: "1923",
+                            died: "1971",
+                            country: "United States"})
+  end
+
+  def setup_iteration_3
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
   end
 
   def test_it_class_exist
@@ -64,5 +87,24 @@ class CuratorTest < Minitest::Test
     @curator.add_photograph(@photo_2)
 
     assert_equal @photo_2, @curator.find_photograph_by_id("2")
+  end
+
+  def test_it_can_find_photographs_by_artist
+    setup_iteration_3
+
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
+  end
+
+  def test_it_can_return_artists_with_multiple_photographs
+    setup_iteration_3
+
+    assert_equal [@artist_3], @curator.artists_with_multiple_photographs
+  end
+
+  def test_it_can_return_photographs_taken_by_artists_from_country
+    setup_iteration_3
+
+    assert_equal [@photo_2, @photo_3, @photo_4], @curator.photographs_taken_by_artists_from("United States")
+    assert_equal [], @curator.photographs_taken_by_artists_from("Argentina")
   end
 end
